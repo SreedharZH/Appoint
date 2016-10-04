491ae7f9b551c4cf52794e11cb47caeca3516313
Hyad Bysal G <hyad.bysal@zohocorp.com>
2016-10-04 17:49:37 +0530

    header

--- app/router.js
+++ app/router.js
@@ -7,53 +7,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('setupWizard', function() {
-    this.route('Businessinfo');
-    this.route('Businesshours');
-    this.route('step1');
-    this.route('step2');
-    this.route('step3');
-    this.route('staff1');
-    this.route('step1-binfo');
-    this.route('step2-bhours');
-    this.route('step3-service');
-    this.route('step4-staff');
-    this.route('1Binfo');
-    this.route('2Bhours');
-    this.route('3Service');
-    this.route('4Staff');
-  });
-
-  this.route('myDesk', function() {
-    this.route('mydesk');
-  });
-
-  this.route('Appointments', function() {
-    this.route('Calendar');
-  });
-
-  this.route('Customers', function() {
-    this.route('Customer');
-  });
-
-  this.route('businessSetup', function() {
-    this.route('binfo');
-    this.route('services');
-    this.route('staff');
-    this.route('notification');
-    this.route('pageDesign');
-    this.route('bookingPreference');
-    this.route('payment');
-    this.route('integration');
-    this.route('service');
-  });
-
-  this.route('businessSet', function() {
-    this.route('service');
+  this.route('setup-wizard', function() {
+    this.route('1-binfo');
+    this.route('2-bhours');
+    this.route('3-service');
+    this.route('4-staff');
   });
 
   this.route('components', function() {
-    this.route('myDesk', function() {
+    this.route('my-desk', function() {
       this.route('mydesk');
     });
 
@@ -69,7 +31,7 @@ Router.map(function() {
       this.route('report');
     });
 
-    this.route('businessSetup', function() {
+    this.route('business-setup', function() {
       this.route('binfo');
       this.route('service');
       this.route('staff');
@@ -81,6 +43,10 @@ Router.map(function() {
       this.route('bsleftmenu');
     });
   });
+
+
+
 });
+
 
 export default Router;
