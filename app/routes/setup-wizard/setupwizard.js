9b14e2805a583ff557d7bbaed20f475824fc2745
Hyad Bysal G <hyad.bysal@zohocorp.com>
2016-10-07 12:51:34 +0530

    tabs

--- app/routes/setup-wizard/setupwizard.js
+++ app/routes/setup-wizard/setupwizard.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+        pressed: function() {
+          $(".steplinks li").click(function(){
+            var findloc = $(this).data('tab');
+            $('.steplinks li').removeClass('active');
+            $(this).addClass('active');
+             $('.tabcontainer').removeClass('active');
+             $("#"+findloc).addClass('active');
+
+
+          })
+
+        }
+    }
 });
